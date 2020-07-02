bbd7408ae3d077f91e150a803366ea9ac44b9f31
Noa Rabin-Cohen <cohenoa33@gmail.com>
2020-07-02 17:41:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,7 +26,7 @@ def get_english_meaning (file, value)
   # code goes here
  library = load_library(file).map do |meaning, lang|  
   	if lang[:japanese] == value
-  	 	return meaning 
+  	 	return return_value = meaning 
     end
       binding.pry
    end
