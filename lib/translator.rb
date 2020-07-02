223437f1bd5a7acf8343b814f9d6e4ca408eed90
Noa Rabin-Cohen <cohenoa33@gmail.com>
2020-07-02 17:36:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,17 @@
 # require modules here
+require "yaml"
+require 'pry'
 
-def load_library
+
+def load_library (file)
   # code goes here
+emoticons_yml = YAML.load_file('lib/emoticons.yml')
+    hash = {}
+    hash.each do |key, value| 
+    hash = (key => {:english => value[0], :japanese => value[1]}}
+    
+      binding.pry
+  end
 end
 
 def get_japanese_emoticon
@@ -11,3 +21,4 @@ end
 def get_english_meaning
   # code goes here
 end
+
